56cf3a244ffbad171fcc6f9b8487ca79694e097d
blondeProgramer <zal22001@byui.edu>
2022-04-23 22:43:05 +0300

    Change JS

--- js/datetime.js
+++ js/datetime.js
@@ -1,2 +1,6 @@
 let infoLastUpd = new Date(document.lastModified);
-document.write('<p> Last update: ' + infoLastUpd + '</p>');
+document.getElementById('change-date').innerHTML = '<p> Last update: ' + infoLastUpd + '</p>';
+
+let thisDate = new Date();
+let thisYear = thisDate.getFullYear();
+document.getElementById('this-year').innerHTML = thisYear;
